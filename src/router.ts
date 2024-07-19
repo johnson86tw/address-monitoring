fcb5c3eeff552d8680471689b245c1c9b3a78aeb
Johnson Chen <chnejohnson@gmail.com>
2024-07-19 15:15:25 +0800

    replace web-push with webcrypto-web-push

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'itty-router';
-import webpush from 'web-push';
+import { buildPushPayload, type PushSubscription, type PushMessage, type VapidKeys } from '@block65/webcrypto-web-push';
 
 // now let's create a router (note the lack of "new")
 const router = Router();
@@ -11,7 +11,7 @@ router.post('/api/save-subscription', async (request, env: Env) => {
 		const timestamp = Date.now();
 		const key = timestamp.toString();
 
-		await env.SUBSCRIPTIONS.put(key, content);
+		await env.SUBSCRIPTIONS.put(key, JSON.stringify(content));
 
 		return new Response(`Saved subscription: ${key}`, { status: 200 });
 	} catch (err: any) {
@@ -21,21 +21,46 @@ router.post('/api/save-subscription', async (request, env: Env) => {
 });
 
 router.post('/api/webhook', async (request, env: Env) => {
-	const content = await request.json();
+	try {
+		const content = await request.json();
 
-	if (content.webhookId !== env.WEBHOOK_ID) {
-		return new Response('Not Authorized', { status: 401 });
+		if (content.webhookId !== env.WEBHOOK_ID) {
+			return new Response('Not Authorized', { status: 401 });
+		}
+
+		const { keys } = await env.SUBSCRIPTIONS.list();
+
+		const vapid: VapidKeys = {
+			subject: undefined,
+			publicKey: undefined,
+			privateKey: env.WEBPUSH_PRIV_KEY,
+		};
+
+		for (const key of keys) {
+			const sub = await env.SUBSCRIPTIONS.get(key.name);
+			if (!sub) continue;
+
+			const subscription: PushSubscription = JSON.parse(sub);
+
+			const message: PushMessage = {
+				data: JSON.stringify(content),
+				options: {
+					ttl: 60,
+				},
+			};
+			const payload = await buildPushPayload(message, subscription, vapid);
+			const res = await fetch(subscription.endpoint, payload);
+
+			if (!res.ok) {
+				throw new Error('Failed to send push notification');
+			}
+		}
+
+		return new Response('Webhook received: ' + JSON.stringify(content));
+	} catch (err: any) {
+		console.error(err);
+		return new Response(err, { status: 500 });
 	}
-
-	const { keys } = await env.SUBSCRIPTIONS.list();
-
-	for (const key of keys) {
-		const subscription = await env.SUBSCRIPTIONS.get(key.name);
-		if (!subscription) continue;
-		webpush.sendNotification(subscription as any, JSON.stringify(content));
-	}
-
-	return new Response('Webhook received: ' + JSON.stringify(content));
 });
 
 // GET collection index
